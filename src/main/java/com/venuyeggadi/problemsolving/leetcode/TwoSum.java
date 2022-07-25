0d341f06919e75c3d2ae2ef51c12a159acde2d61
Venu Yeggadi <yeggadivenu@gmail.com>
2022-07-25 08:02:13 +0530

    Add Two Sum problems from leetcode

--- src/main/java/com/venuyeggadi/problemsolving/leetcode/TwoSum.java
+++ src/main/java/com/venuyeggadi/problemsolving/leetcode/TwoSum.java
@@ -1,7 +1,5 @@
 package com.venuyeggadi.problemsolving.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers
@@ -11,27 +9,31 @@ import java.util.Map;
  * You can return the answer in any order.
 
  * Example 1:
-   Input: nums = [2,7,11,15], target = 9
-   Output: [0,1]
-   Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
  * Example 2:
-   Input: nums = [3,2,4], target = 6
-   Output: [1,2]
-   
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
+
  * Example 3:
-   Input: nums = [3,3], target = 6
-   Output: [0,1]
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
 
  * Constraints:
-   * 2 <= nums.length <= 10^4
-   * -10^9 <= nums[i] <= 10^9
-   * -10^9 <= target <= 10^9
-   * Only one valid answer exists.
+    * 2 <= nums.length <= 10^4
+    * -10^9 <= nums[i] <= 10^9
+    * -10^9 <= target <= 10^9
+    * Only one valid answer exists.
  */
 
 
-//Bruteforce Time : O(n^2) , Space : O(1)
+import java.util.HashMap;
+import java.util.Map;
+
+// Solution 1
+// Bruteforce Time : O(n^2) , Space : O(1)
 class TwoSumSolution1 {
     public int[] twoSum(int[] nums, int target) {
         boolean found = false;
@@ -50,7 +52,7 @@ class TwoSumSolution1 {
     }
 }
 //Same Bruteforce with optimised return statement
-class TwoSumSolution2 {
+class TwoSumSolution1WithBetterReturnStatement {
     public int[] twoSum(int[] nums, int target) {
         boolean found = false;
         int i = 0, j = 0;
@@ -66,6 +68,7 @@ class TwoSumSolution2 {
 }
 
 
+// solution 2
 //Using HashMap(Dictionary) Time : O(n), Space : O(n)
 /**
  * Time complexity : O(n). We traverse the list containing n elements only once.
@@ -73,17 +76,17 @@ class TwoSumSolution2 {
  * Space complexity : O(n). The extra space required depends on the number of
        items stored in the hash table, which stores at most n elements.
 */
-class TwoSumSolution3 {
+class TwoSumSolution2 {
     public int[] twoSum(int[] nums, int target) {
     	Map<Integer, Integer> map = new HashMap<>();
     	int complement;
-        for(int i = 0; i < nums.length; i++) {
-        	complement = target - nums[i];
+        for(int index = 0; index < nums.length; index++) {
+        	complement = target - nums[index];
         	if(map.containsKey(complement))
-        		return new int[] { map.get(complement), i};
-        	//else (optinal);
-        	map.put(nums[i], i);
+        		return new int[]{ map.get(complement), index };
+        	//else (optional);
+        	map.put(nums[index], index);
         }
-        return new int[]{-1, -1};
+        return null;
     }
 }
